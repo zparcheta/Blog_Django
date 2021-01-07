303e72e7bb9c32ec88a16ae3f22aeaaf76990f62
zparcheta_mac <zparcheta_mac>
2021-01-07 12:01:16 +0100

    implement views

---
+++ blog_project/blog_app/urls.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+
+from django.urls import path
+from . import views
+
+app_name = 'blog_app'
+
+urlpatterns = [
+    # post views
+    path('', views.post_list, name='post_list'),
+    path('<int:year>/<int:month>/<int:day>/<slug:post>/',
+    views.post_detail,
+    name='post_detail'),
+]
