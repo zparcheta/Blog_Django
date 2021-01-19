a07fd9928ca5d3b185dec483df38ee74248c151f
zparcheta <zuza03@gmail.com>
2021-01-19 10:53:40 +0100

    last featured

--- blog_project/blog_app/models.py
+++ blog_project/blog_app/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
 from django.urls import reverse
+from taggit.managers import TaggableManager
 
 class PublishedManager(models.Manager):
     def get_queryset(self):
@@ -28,6 +29,7 @@ class Post(models.Model):
         default='draft')
     objects = models.Manager() # The default manager.
     published = PublishedManager() # Our custom manager.
+    tags = TaggableManager()
 
     def get_absolute_url(self):
         return reverse('blog:post_detail', args=[self.publish.year,
@@ -39,4 +41,21 @@ class Post(models.Model):
         def __str__(self):
             return self.title
 
+
+class Comment(models.Model):
+    post = models.ForeignKey(Post,
+                             on_delete=models.CASCADE,
+                             related_name='comments')
+    name = models.CharField(max_length=80)
+    email = models.EmailField()
+    body = models.TextField()
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    active = models.BooleanField(default=True)
+
+    class Meta:
+        ordering = ('created',)
+
+    def __str__(self):
+        return f'Comment by {self.name} on {self.post}'
 
